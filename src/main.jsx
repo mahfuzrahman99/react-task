c1196df751a0d135242882fde0e24d354b1d2bf1
Mahfuzur Rahman Shabbir <mahfuzurrahmanshabbir@gmail.com>
2024-02-19 20:31:53 +0600

    problem-1 is successfully done

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import '../src/assets/scss/App.scss';
-import {BrowserRouter} from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "../src/assets/scss/App.scss";
+import { BrowserRouter } from "react-router-dom";
+import StateProvider from "./Context/StateProvider";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-      <BrowserRouter>
-          <App />
-      </BrowserRouter>
+    <BrowserRouter>
+      <StateProvider>
+        <App />
+      </StateProvider>
+    </BrowserRouter>
   </React.StrictMode>
-)
+);
